b30df14309c0a40af08b24c02e9bc94df0e82f89
Gloriana Hernandez <U91597@ust-global.com>
2019-01-25 09:50:13 -0600

    challenge 4 result

--- data.js
+++ data.js
@@ -1,26 +1,28 @@
 var students = [
-    {id: "1122222", name: "John", score: 90 }, // 0 {Object} => students[0] => students[0].score => total  
-    {id: "2223333", name: "Larry", score: 60 }, // 1
-    {id: "4455555", name: "Joseph", score: 50 }, // 2
-    {id: "5526666", name: "Karla", score: 80}
+    {name: "John", score: 10, id: "A100"}, // 0 {Object} => students[0] => students[0].score => total  
+    {name: "Larry", score: 60, id: "A101"}, // 1
+    {name: "Joseph", score: 50, id: "A102"}, // 2
+    {name: "Karla", score: 100, id: "A103"}, //3
 ];
 
-document.write("<h3>JSON</h3>");
-document.write("<pre class='alert alert-secondary'>"); // 1) Bootstrap class
-document.write(JSON.stringify(students, undefined, 2));
+//document.writeln(students.length);
+
+document.write("<pre class= 'alert alert-secondary'>");
+document.write(JSON.stringify(students));
 document.write("</pre>");
-document.write("<br/>");
+
 var names = [];
 
 students.forEach(student => {
     names.push(student.name);
 });
 
-document.writeln("Students are:" + names);
+document.writeln(names);
 
-document.write("<pre>");
-document.writeln(`Average: ${calculateAverage()}`);
-document.write("</pre>");
+//document.write("<pre>");
+//document.writeln("Average: " + calculateAverage());
+//document.writeln(`Average: ${calculateAverage()}`);
+//document.write("</pre>");
 
 function calculateAverage(){
 
@@ -33,65 +35,6 @@ function calculateAverage(){
     return average;
 }
 
-function loadDataGrid() {
-
-    var i = 0;
-    let dataList = document.getElementById("dataList");
-    
-    while (i < students.length)
-    {
-        var listItem = document.createElement("section");
-        listItem.classList.add("row");
-        
-        var id = document.createElement("div");
-        id.classList.add("col-sm");
-        id.innerText =  students[i].id;
-
-        var name = document.createElement("div");
-        name.classList.add("col-sm");
-        name.innerText =  students[i].name;
-
-        var score = document.createElement("div");
-        score.classList.add("col-sm");
-        score.innerText =  students[i].score;
-
-        console.log(students[i]);
-
-        dataList.appendChild(listItem);
-
-        listItem.appendChild(id);
-        listItem.appendChild(name);
-        listItem.appendChild(score);
-
-        i = i + 1; // Alternatively, use i++;
-
-        // Other ways:
-        // i += 2;
-        // i += 3;
-    }
-}
-
-function displayAverage()
-{
-    var resultSection = document.getElementById("resultSection");
-    var paragraph = document.createElement("p");
-    paragraph.classList.add("badge"); // 2) Bootstrap classes
-    paragraph.classList.add("badge-info");
-
-    paragraph.innerText = "Average: " + calculateAverage();
-
-    resultSection.appendChild(paragraph);
-}
-
-function myReplacer(name, val) {
-    if (typeof val === 'string') {
-        return val.toString().toUpperCase();  
-     } else {
-        return val; // return as is
-    }
-};
-
-// Old-way of loading data (ol). No longer used
 function loadData(){
 
     var i = 0;
@@ -111,4 +54,60 @@ function loadData(){
         // i += 2;
         // i += 3;
     }
+}
+
+function displayAverage()
+{
+    var resultSection = document.getElementById("resultSection");
+    var paragraph = document.createElement("p");
+    paragraph.classList.add("badge");
+    paragraph.classList.add("badge-pill");
+    paragraph.classList.add("badge-secondary");
+    paragraph.innerText = "Average: " + calculateAverage();
+
+    resultSection.appendChild(paragraph);
+}
+
+function myReplacer(name, val) {
+    if (typeof val === 'string') {
+        return val.toString().toUpperCase();  
+     } else {
+        return val; // return as is
+    }
+};
+
+function fillTable(){
+    var table = document.getElementById("grid");
+
+    students.forEach(student => {
+        var row = document.createElement('section') 
+        row.className = 'row'
+        
+        var column1 = document.createElement('section')
+        var column2 = document.createElement('section')
+        var column3 = document.createElement('section')
+        column1.className = "col-sm"
+        column2.className = "col-sm"
+        column3.className = "col-sm"
+
+        table.append(row);
+        row.appendChild(column1);
+        column1.innerText = student.name;
+        row.appendChild(column2);
+        column2.innerText = student.score;
+        row.appendChild(column3);
+        column3.innerText = student.id;
+
+        if(student.score <60){
+            column1.classList.add("badge");
+            column1.classList.add("badge-pill");
+            column1.classList.add("badge-danger");
+            column2.classList.add("badge");
+            column2.classList.add("badge-pill");
+            column2.classList.add("badge-danger");
+            column3.classList.add("badge");
+            column3.classList.add("badge-pill");
+            column3.classList.add("badge-danger");
+        }
+    });
 }
